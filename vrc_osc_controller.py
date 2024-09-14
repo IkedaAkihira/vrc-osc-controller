261618554644d167c464af95ea871c3786ffc508
IkedaAkihira <ikedaakihira@users.noreply.github.com>
2024-09-14 21:16:31 +0900

    公開用に整理

--- vrc_osc_controller.py
+++ vrc_osc_controller.py
@@ -1,18 +1,15 @@
 from pynput.mouse import Button, Listener
 from pythonosc import udp_client
 
-ip = '192.168.0.10'
+ip = input('IPアドレスを入力してください: ')
 port = 9000
 left_click_parameter_name = 'left_click'
 right_click_parameter_name = 'right_click'
 middle_click_parameter_name = 'middle_click'
-scroll_parameter_name = 'attack_type'
-scroll_parameter_count = 2
 
 client = udp_client.SimpleUDPClient(ip, port)
 
 def on_click(x, y, button, pressed):
-    print(f'Click: {button}, {pressed}')
     if button == Button.left:
         click_parameter_name = left_click_parameter_name
     elif button == Button.right:
@@ -22,14 +19,5 @@ def on_click(x, y, button, pressed):
 
     client.send_message(f'/avatar/parameters/{click_parameter_name}', pressed)
 
-scroll_num = 0
-
-def on_scroll(x, y, dx, dy):
-    print('Scroll')
-    global scroll_num
-    scroll_num += dy
-    scroll_num %= scroll_parameter_count
-    client.send_message(f'/avatar/parameters/{scroll_parameter_name}', scroll_num)
-
-with Listener(on_click=on_click, on_scroll=on_scroll) as listener:
+with Listener(on_click=on_click) as listener:
     listener.join()
